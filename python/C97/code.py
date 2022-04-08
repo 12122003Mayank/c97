c6bb2620a959f256e8efadaa3612d744e7b0d8c1
12122003Mayank <78353413+12122003Mayank@users.noreply.github.com>
2022-04-08 18:51:45 +0530

    Add files via upload

---
+++ python/C97/code.py
@@ -0,0 +1,13 @@
+#a=25.5
+#print(type(a))
+friends=["ashmit","granth","mayank","aditya","aaryaman"]
+#print(friends[3])
+# for f in friends:
+#     print(f)
+age=10
+if(age<=12):
+    print("kid")
+elif(age<18 and age>12):        
+    print("teenager")
+elif(age>=18):
+    print("adult")    
